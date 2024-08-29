9248e5f67400ba1d92467762d8e2e52e7135c8c8
paoolg1225 <psgomez1229@gmail.com>
2024-08-29 00:32:28 -0700

    Initial commit of Snake Game

---
+++ src/SnakeFrame.java
@@ -0,0 +1,20 @@
+import javax.swing.JFrame;
+
+
+public class SnakeFrame extends JFrame{
+
+	
+
+		SnakeFrame(){
+			this.add(new SnakePanel());
+			this.setTitle("Snake");
+			this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			this.setResizable(false);
+			this.pack();
+			this.setVisible(true);
+			this.setLocationRelativeTo(null);
+		}
+
+	}
+
+
